739397f100e9643bb30c875134002b5ba15f8316
Debatreya <debatreyadas@gmail.com>
2025-01-11 20:06:10 +0530

    fix(css): Task

--- apps/excalibur/src/App.jsx
+++ apps/excalibur/src/App.jsx
@@ -1,48 +1,62 @@
-import './App.css'
-import Background from './Background/Background'
-import Countdown from './Countdown/Countdown'
-import Navbar from './navbar/Navbar'
-import Event from './Event/Event'
-import TimeLine from './Timeline/Timeline'
-import Themes from './Themes/Themes'
+import "./App.css";
+import Background from "./Background/Background";
+import Countdown from "./Countdown/Countdown";
+import Navbar from "./navbar/Navbar";
+import Event from "./Event/Event";
+import TimeLine from "./Timeline/Timeline";
+import Themes from "./Themes/Themes";
 // import Sponsors from './Sponsors/Sponsors'
-import Footer from './Footer/Footer'
-import Rules from './Rules/Rules'
-import ContactUs from './ContactUs/ContactUs'
+import Footer from "./Footer/Footer";
+import Rules from "./Rules/Rules";
+import ContactUs from "./ContactUs/ContactUs";
 
 function App() {
-  return (
-    <div className="w-[90%] h-screen flex flex-col items-center mx-auto overflow-x-hidden overflow-y-scroll scroll-smooth snap-y snap-mandatory">
-
-      <section className="min-h-[100svh] w-full flex flex-col justify-start items-center scroll-snap-align-start">
-        <Navbar />
-        <h1 className="ml-8 inline p-0 text-center text-[#67326C] text-[13.25rem] font-pirata tracking-[2.0625rem]  transition-all duration-500 text-shadow-pink">EXCALIBUR</h1>
-        <div className="font-starlord text-[2.5rem] text-[rgba(255,255,255,0.797)] mt-[-1.5rem] mb-[1.5rem] tracking-[1.5px]
+	return (
+		<div className="w-[90%] h-screen flex flex-col items-center mx-auto overflow-x-hidden overflow-y-scroll scroll-smooth snap-y snap-mandatory">
+			<section className="min-h-[100svh] w-full flex flex-col justify-start items-center scroll-snap-align-start">
+				<Navbar />
+				<h1 className="ml-8 inline p-0 text-center text-[#67326C] text-[13.25rem] font-pirata tracking-[2.0625rem]  transition-all duration-500 text-shadow-pink">
+					EXCALIBUR
+				</h1>
+				<div
+					className="font-starlord text-[2.5rem] text-[rgba(255,255,255,0.797)] mt-[-1.5rem] mb-[1.5rem] tracking-[1.5px]
         max-xs:text-[1rem]  max-xs:mt-[1.5rem] max-xs:tracking-[1.5px] max-xs:mb-[1.5rem] max-sm:text-[1.3rem] max-sm:mt-[0.5rem] max-sm:mb-[0.5rem] max-sm:tracking-[0.8px]
-        max-md:text-[2rem] max-md:mt-[-1rem] max-md:mb-[1rem] max-md:tracking-[1.5px]">DESIGN | DEVELOP | DOMINATE</div>
-        <Countdown />
-        {/* <Sponsors /> */}
-      </section>
+        max-md:text-[2rem] max-md:mt-[-1rem] max-md:mb-[1rem] max-md:tracking-[1.5px]"
+				>
+					DESIGN | DEVELOP | DOMINATE
+				</div>
+				<Countdown />
+				{/* <Sponsors /> */}
+			</section>
 
-      <section className="min-h-[100svh] flex flex-col justify-center items-center scroll-snap-align-center" id="about">
-        <Event />
-        <section id="themes">
-        <Themes />
-        </section>
-      </section>
+			<section
+				className="min-h-[100svh] flex flex-col justify-center items-center scroll-snap-align-center"
+				id="about"
+			>
+				<Event />
+				<section id="themes">
+					<Themes />
+				</section>
+			</section>
 
-      <section className="min-h-fit flex flex-col justify-center items-center scroll-snap-align-start" id="schedule">
-        <TimeLine />
-      </section>
-      <section className="min-h-[100svh] flex flex-col justify-around items-center gap-6 p-6 scroll-snap-align-center" id="rules">
-        <Rules />
-        <ContactUs />
-        <Footer />
-      </section>
+			<section
+				className="min-h-fit flex flex-col justify-center items-center scroll-snap-align-start"
+				id="schedule"
+			>
+				<TimeLine />
+			</section>
+			<section
+				className="min-h-[100svh] flex flex-col justify-around items-center gap-6 p-6 scroll-snap-align-center"
+				id="rules"
+			>
+				<Rules />
+				<ContactUs />
+				<Footer />
+			</section>
 
-      <Background />
-    </div>
-  )
+			<Background />
+		</div>
+	);
 }
 
-export default App
+export default App;
