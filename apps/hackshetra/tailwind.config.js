2fd1654fd7229f3e787984cd2e379d9c2ad9c9f1
Yash Shakya <yashshakya40@gmail.com>
2025-01-12 14:24:44 +0530

    feat(hackshetra/app.jsx): background of hackshetra website

---
+++ apps/hackshetra/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'map': "url('./assets/map.svg')",
+      },
+      backgroundSize: {
+        '60%': '60%',
+      },
+      "fontFamily": {
+        "porter-sans-block": "Porter Sans Block"
+      }
+    }
+  },
+  plugins: [],
+}
+
