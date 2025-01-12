da18c9b791154fb0bd05f440fb24e743fef6b604
Yash Shakya <yashshakya40@gmail.com>
2025-01-13 00:08:50 +0530

    style(hackshetra-navbar.jsx): aligned the navbar

--- apps/hackshetra/src/App.jsx
+++ apps/hackshetra/src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import techSpardhaLogo from "./assets/techspardhaLogo.svg"
+import Navbar from './components/Navbar.jsx'
 
 function App() {
 
   return (
     <>
-      <div className={`min-h-screen h-min w-screen absolute bg-map bg-no-repeat bg-fixed bg-60% bg-center md:bg-bottom `}>
-        <div className='flex justify-between items-center p-4 mx-8 -mb-10 sm:mx-14 md:mx-20 lg:mx-32'>
-          <div className='text-white text-6xl text-center'>{"<"}</div>
-          <img src={techSpardhaLogo} alt="techSpardhaLogo" />
-        </div>
-        <div className="text-[#8FFFFF] text-center text-4xl md:text-6xl font-porter-sans-block tracking-wide">HACKSHETRA</div>
-      </div>
+    <div className={`min-h-screen h-min w-screen absolute bg-map bg-no-repeat bg-fixed bg-60% bg-center md:bg-bottom`}>
+      <Navbar/>
+      
+      <h1 className="text-[#8FFFFF] text-center text-4xl md:text-6xl font-porter-sans-block tracking-wide">HACKSHETRA</h1>
+    </div>
+
     </>
   )
 }
