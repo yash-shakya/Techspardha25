b14cd9684f6d56fea4b5ba74aa602101fc983e59
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2025-01-17 20:35:12 +0530

    fix: center in mobile view

--- apps/excalibur/src/navbar/Navbar.jsx
+++ apps/excalibur/src/navbar/Navbar.jsx
@@ -107,30 +107,46 @@ export default function Navbar() {
     let back = '<';
     return (
         <nav className="text-white font-kode mt-12 mb-auto w-[95%] flex justify-between transition-[duration] duration-[500ns] relative h-[60px]">
-            <a className='relative flex justify-center items-center my-auto'>
+            <a className='relative hidden md:flex justify-center items-center my-auto'>
                 <span className="text-[4rem] my-auto">{back}</span>
             </a>
             <Normalnav />
             <Dropnav />
-            <span className="techspardha_logo relative flex justify-center items-center my-auto"
-                style={{ filter: 'drop-shadow(0px 0px 20px rgb(54, 124, 255))' }}>
-                <svg
-                    className="absolute top-[1.0585rem] w-[6.71369rem] h-[2.3125rem] fill-[#367CFF]"
-                    style={{ filter: 'blur(40.58064270019531px)' }}
-                    xmlns="http://www.w3.org/2000/svg" width="271" height="201" viewBox="0 0 271 201" fill="none">
-                    <g filter="url(#filter0_f_2250_4319)">
-                        <ellipse cx="135.71" cy="100.436" rx="53.7097" ry="18.5" fill="#367CFF" fillOpacity="0.7" />
-                    </g>
-                    <defs>
-                        <filter id="filter0_f_2250_4319" x="0.838715" y="0.774261" width="269.742" height="199.323" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                            <feGaussianBlur stdDeviation="40.5806" result="effect1_foregroundBlur_2250_4319" />
-                        </filter>
-                    </defs>
-                </svg>
-                <img src="/images/TechspardhaLogo.svg" alt="T" />
-            </span>
+            <span 
+    className="techspardha_logo relative md:flex justify-center items-center my-auto hidden lg:flex"
+    style={{ filter: 'drop-shadow(0px 0px 20px rgb(54, 124, 255))' }}
+>
+    <svg
+        className="absolute top-[1.0585rem] w-[6.71369rem] h-[2.3125rem] fill-[#367CFF]"
+        style={{ filter: 'blur(40.58064270019531px)' }}
+        xmlns="http://www.w3.org/2000/svg"
+        width="271"
+        height="201"
+        viewBox="0 0 271 201"
+        fill="none"
+    >
+        <g filter="url(#filter0_f_2250_4319)">
+            <ellipse cx="135.71" cy="100.436" rx="53.7097" ry="18.5" fill="#367CFF" fillOpacity="0.7" />
+        </g>
+        <defs>
+            <filter
+                id="filter0_f_2250_4319"
+                x="0.838715"
+                y="0.774261"
+                width="269.742"
+                height="199.323"
+                filterUnits="userSpaceOnUse"
+                colorInterpolationFilters="sRGB"
+            >
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                <feGaussianBlur stdDeviation="40.5806" result="effect1_foregroundBlur_2250_4319" />
+            </filter>
+        </defs>
+    </svg>
+    <img src="/images/TechspardhaLogo.svg" alt="T" />
+</span>
+
         </nav>
     );
 }
