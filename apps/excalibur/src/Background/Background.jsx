a647af2b77987340d7f6bb445e4ff3129c33fae0
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2025-01-17 21:41:54 +0530

    Background image corrected

--- apps/excalibur/src/Background/Background.jsx
+++ apps/excalibur/src/Background/Background.jsx
@@ -2,14 +2,27 @@
 export default function Background(){
     return(
         <>
-            <div className="overflow-clip fixed w-[100svw] mt-auto bottom-0 mb-[-10px] z-[-9]">
-                <img src="/images/excalibur_background_svg.svg" alt="" 
-                className='w-full max-xs:scale-[3] max-sm:scale-[2] max-md:scale-[1.5]'/>
-            </div>
-            <div className="overflow-clip fixed w-[100svw] z-[-11]">
-                <img src="/images/Home_Page.jpg" alt="" 
-                className='h-[100svh] min-w-[100svw]'/>
-            </div>
+        <div className="overflow-hidden fixed w-[100svw] bottom-0 z-[-9]">
+    <img 
+        src="/images/excalibur_background_svg.svg" 
+        alt="" 
+        className="
+            w-full h-auto
+            object-cover 
+            max-xs:scale-[4] max-xs:translate-y-[-30%] 
+            max-sm:scale-[2] max-sm:translate-y-[10%] 
+            max-md:scale-[1.5] 
+        "
+    />
+</div>
+<div className="overflow-hidden fixed w-[100svw] z-[-11]">
+    <img 
+        src="/images/Home_Page.jpg" 
+        alt="" 
+        className="w-full h-[100svh] object-cover object-center"
+    />
+</div>
+
         </>
     )
 }
