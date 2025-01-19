6d91cc570fedcf03376d74abfaf67a25d8010e54
suyash1312shukla <123103079@nitkkr.ac.in>
2025-01-19 20:15:30 +0530

    fix(Techspardha): fix hydration error for nested <a> tags

--- apps/web/app/ui/components/EventCard.tsx
+++ apps/web/app/ui/components/EventCard.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export interface EventCardProps {
   name: string;
@@ -9,6 +8,11 @@ export interface EventCardProps {
 }
 
 export default function EventCard({ name, img, isActive, id }: EventCardProps) {
+
+  const handleUrl = () => {
+    window.location.href = `/events/${id}`;
+  } 
+
   return (
     <div
       className={`flex flex-col rounded-md border-[.5px] md:h-[80%] border-[#00629666] bg-[#012436] transition-transform duration-300 
@@ -16,8 +20,8 @@ export default function EventCard({ name, img, isActive, id }: EventCardProps) {
         isActive ? "scale-95 shadow-lg" : ""
       }`}
     >
-      <div className="relative flex items-center justify-center w-full h-36 sm:h-80 md:h-96 overflow-hidden">
-        <Link href={`/events/${id}`}>
+      <div className="relative flex items-center justify-center w-full h-36 sm:h-80 md:h-96 overflow-hidden" onClick={()=>handleUrl()}>
+        
           <Image
             className="object-cover border-[1px] border-white rounded-lg"
             src={`/${img}`}
@@ -28,7 +32,7 @@ export default function EventCard({ name, img, isActive, id }: EventCardProps) {
                    (max-width: 1200px) 60vw,
                    50vw"
           />
-        </Link>
+        
       </div>
       <div className="text-white text-center p-2 font-[Satoshi Variable] text-lg sm:text-3xl">
         {name}
