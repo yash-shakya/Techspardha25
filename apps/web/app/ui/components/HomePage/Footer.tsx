7446569e4d6702ac50fd7e115ce233192752fea5
suyash1312shukla <123103079@nitkkr.ac.in>
2025-01-21 19:42:11 +0530

    refactor(Techspardha): improve UI responsiveness

--- apps/web/app/ui/components/HomePage/Footer.tsx
+++ apps/web/app/ui/components/HomePage/Footer.tsx
@@ -1,21 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
-import { FaDiscord, FaLinkedinIn, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
+import {
+  FaDiscord,
+  FaLinkedinIn,
+  FaTwitter,
+  FaYoutube,
+  FaInstagram,
+} from "react-icons/fa";
 import { socials } from "../../../constants/Socials";
 
 export default function Footer() {
   return (
     <footer className="relative bg-transparent py-8 px-4">
-      {/* <div
-        className="absolute w-screen mt-10  inset-0 -z-10"
+      {/* may comment this based on UI */}
+      <div
+        className="absolute w-screen mt-10 inset-0 -z-10 bg-no-repeat bg-center md:bg-contain"
         style={{
           backgroundImage: "url('/Home/footer-grid.png')",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-          backgroundPosition: "center",
         }}
-      ></div> */}
+      ></div>
 
+      {/* may comment this based on UI */}
       <div className="container mx-auto flex flex-col items-center ">
         <div className="w-72 h-32 md:w-96 md:h-32 relative">
           <Image
@@ -27,8 +32,8 @@ export default function Footer() {
         </div>
 
         <p className="text-white text-xs md:text-sm flex items-center gap-1 pt-2 border-t-2 mb-5 border-[#00a6ff66]">
-          Developed with{" "}
-          <span className="text-red-500 animate-pulse">❤️</span> by Technobyte
+          Developed with <span className="text-red-500 animate-pulse">❤️</span>{" "}
+          by Technobyte
         </p>
 
         <nav className="flex gap-6 items-center">
