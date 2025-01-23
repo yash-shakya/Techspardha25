e897c60613334ce1d035f76305c845fcd9907574
Debatreya <debatreyadas@gmail.com>
2025-01-23 23:48:26 +0530

    feat: service.ts added in webs
    This file can be used to fetch data from database
    commit by zolo-ryan
    profile: github.com/zolo-ryan

---
+++ apps/web/app/server/actions/constants.ts
@@ -0,0 +1,89 @@
+export type Event = {
+    coordinators: Coordinator[];
+    description: string;
+    document: string;
+    endTime: number;
+    eventName: string;
+    flagship: boolean;
+    poster: string;
+    rules: string[];
+    startTime: number;
+    venue: string;
+};
+
+export type EventMap = {
+    [category: string]: {
+        [eventName: string]: Event;
+    };
+};
+
+type Coordinator = {
+    coordinator_name: string;
+    coordinator_number: string;
+};
+
+export type Sponsor = {
+    category: string;
+    alt: string;
+    imageUrl: string;
+    name: string;
+    targetUrl: string;
+    id: string;
+};
+
+export type TechspardhaTeam = {
+    contacts: Contacts[];
+    logo: string | null;
+    teamName: string;
+};
+
+type Contacts = {
+    imageURL: string;
+    name: string;
+    post: Post | string;
+};
+
+enum Post {
+    Convenor = "Convenor",
+    CoConvenor = "Co-Convenor",
+}
+
+export type TechspardhaTeamsDTO = {
+    [key: string]: TechspardhaTeam;
+}
+
+export type Notification = {
+    android_channel_id: string;
+    body: string;
+    image: string;
+    link: string;
+    title: string;
+    time: number;
+};
+
+export type NotificationsDTO = {
+    [key: string]: Notification;
+};
+
+export type Lecture = {
+    id: string;
+    date: string;
+    desc: string;
+    facebook: string;
+    imageUrl: string;
+    insta: string;
+    linkedin: string;
+    link: string;
+    name: string;
+    time: string;
+};
+
+export type GetAllDetailedEvents = {
+    [eventCategory: string]: {
+        [eventName: string]: Event
+    }
+}
+
+export type GetAllNotifications = {
+    [id: string]: Notification;
+}
