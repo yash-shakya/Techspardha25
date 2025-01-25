fe4ea4774dc86c959801f4f1b542d04dcc239932
Debatreya <debatreyadas@gmail.com>
2025-01-25 19:42:15 +0530

    feat(teams): Themes cards done, UX may change

--- apps/web/app/constants/devpage.ts
+++ apps/web/app/constants/devpage.ts
@@ -1,62 +1,56 @@
 // App dev page constants
 
 export const DEVS = [
-    {
-        id: "aboutAppDevs",
-        members: [
-            {
-                name: "Utkarsh Swaroop Srivastava",
-                year: "Pre-Final",
-                github: "https://github.com/Debatreya",
-                insta: "https://www.instagram.com/debatreya_das/",
-                linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
-                imageUrl: "https://i.ibb.co/4MYfDJh/Debatreya-img.jpg"
-            },
-            {
-                name: "Santa Clause",
-                year: "Final",
-                github: "https://github.com/Debatreya",
-                insta: "https://www.instagram.com/debatreya_das/",
-                linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
-                imageUrl: "https://i.ibb.co/4MYfDJh/Debatreya-img.jpg"
-            }
-        ]
-    },
-    {
-        id: "aboutDevs",
-        members: [
-            {
-                name: "Debatreya Das",
-                year: "Pre-Final",
-                github: "https://github.com/Debatreya",
-                insta: "https://www.instagram.com/debatreya_das/",
-                linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
-                imageUrl: "https://i.ibb.co/4MYfDJh/Debatreya-img.jpg"
-            },
-            {
-                name: "Steve Smith",
-                year: "Sophomore",
-                github: "https://github.com/Debatreya",
-                insta: "https://www.instagram.com/debatreya_das/",
-                linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
-                imageUrl: "https://i.ibb.co/R7tqLWf/santaGO.png"
-            },
-            {
-                name: "Uncanny",
-                year: "Freshman",
-                github: "https://github.com/Debatreya",
-                insta: "https://www.instagram.com/debatreya_das/",
-                linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
-                imageUrl: "https://i.ibb.co/cgD6wmy/uncanny-Devs.png"
-            }
-        ]
-    }    
-]
+	{
+		id: "aboutAppDevs",
+		members: [
+			{
+				name: "Utkarsh Swaroop Srivastava",
+				year: "Pre-Final",
+				github: "https://github.com/Debatreya",
+				insta: "https://www.instagram.com/debatreya_das/",
+				linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
+				imageUrl: "https://i.ibb.co/4MYfDJh/Debatreya-img.jpg",
+			},
+			{
+				name: "Santa Clause",
+				year: "Final",
+				github: "https://github.com/Debatreya",
+				insta: "https://www.instagram.com/debatreya_das/",
+				linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
+				imageUrl: "https://i.ibb.co/4MYfDJh/Debatreya-img.jpg",
+			},
+		],
+	},
+	{
+		id: "aboutDevs",
+		members: [
+			{
+				name: "Debatreya Das",
+				year: "Pre-Final",
+				github: "https://github.com/Debatreya",
+				insta: "https://www.instagram.com/debatreya_das/",
+				linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
+				imageUrl: "https://i.ibb.co/4MYfDJh/Debatreya-img.jpg",
+			},
+			{
+				name: "Steve Smith",
+				year: "Sophomore",
+				github: "https://github.com/Debatreya",
+				insta: "https://www.instagram.com/debatreya_das/",
+				linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
+				imageUrl: "https://i.ibb.co/R7tqLWf/santaGO.png",
+			},
+			{
+				name: "Uncanny",
+				year: "Freshman",
+				github: "https://github.com/Debatreya",
+				insta: "https://www.instagram.com/debatreya_das/",
+				linkedin: "https://www.linkedin.com/in/debatreya-das-895517204/",
+				imageUrl: "https://i.ibb.co/cgD6wmy/uncanny-Devs.png",
+			},
+		],
+	},
+];
 
-export const THEMES = [
-    "blue",
-    "red",
-    "green",
-    "cyan",
-    "orange"
-]
+export const THEMES = ["blue", "red", "green", "cyan", "orange"];
---
+++ apps/web/app/ui/components/TeamCard.tsx
@@ -0,0 +1,69 @@
+// Purpose: A component that displays a card with information about a developer.
+
+import Image from "next/image";
+
+interface Contacts {
+	name: string;
+	post: string;
+	imageURL?: string;
+}
+
+interface ContactCardProps {
+	contact: Contacts;
+}
+
+// Component that displays a card with information about a developer.
+export default function ContactCard({
+	contact,
+}: ContactCardProps) {
+    let theme = "slate";
+    if(contact.post === "Convenor") {
+        theme = "stone";
+    }
+
+    
+	return (
+		<>
+			<div
+				className={`max-w-[350px] min-w-[300px] h-[500px] rounded-2xl relative shadow-lg shadow-${theme}-900`}
+			>
+				<div
+					className={`h-[55%] bg-${theme}-500 rounded-t-2xl text-gray-900 flex flex-wrap items-center justify-center relative`}
+				>
+					{/* A small notch */}
+					<div className="absolute w-[100px] h-3 bg-gray-800 rounded-3xl top-3"></div>
+					{/* Contact Name */}
+					<div
+						className={`${
+							contact.name.length > 20 ? "sm:text-4xl text-3xl" : "sm:text-5xl text-4xl"
+						} font-black text-center flex flex-wrap items-center justify-center w-fit mx-auto mb-[7rem] bg-gradient-to-b from-gray-700 to-gray-950 text-transparent bg-clip-text p-2 filter drop-shadow-sm`}
+					>
+						{contact.name}
+					</div>
+				</div>
+				{/* Contact Image */}
+				<div
+					className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-400 rounded-full w-[200px] h-[200px] shadow-md shadow-${theme}-500`}
+				>
+					<Image
+						src={contact.imageURL ?? "/dev.png"}
+						alt={contact.name}
+						// width={200}
+						// height={200}
+                        fill={true}
+                        objectFit="cover"
+						className="rounded-full"
+					/>
+				</div>
+				<div className="h-[45%] bg-gray-900 rounded-b-2xl pt-5 flex flex-col gap-3">
+					<div className="p-4 grow flex flex-col">
+						{/* Conatct Post */}
+						<div className={`text-3xl text-${theme}-500 my-auto text-center ${contact.post === "Convenor" ? "font-bold" : ""}`}>
+							{contact.post}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
