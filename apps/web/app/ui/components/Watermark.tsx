69b6df3c2eeb351550211102bb76a93e7f5180b9
Debatreya <debatreyadas@gmail.com>
2025-02-02 19:21:09 +0530

    feat(teams): Techspardha Teams carousal logos

--- apps/web/app/ui/components/Watermark.tsx
+++ apps/web/app/ui/components/Watermark.tsx
@@ -7,9 +7,10 @@ export default function Watermark() {
 	const teamArray = Object.values(TEAMS);
 	return (
 		<div className="w-full overflow-hidden flex flex-col justify-start items-center gap-3 mt-9">
-			<div className="w-[230px] text-center text-[#d2d6db]/60 text-base font-medium font-['Satoshi Variable'] leading-snug">
+			{/* TODO: Its not looking Good */}
+			{/* <div className="w-[230px] text-center text-[#d2d6db]/60 text-base font-medium font-['Satoshi Variable'] leading-snug">
 				Team Techspardha 🙌🏼
-			</div>
+			</div> */}
 
 			<div className="w-full h-fit relative flex-col justify-start items-start">
 				<Carousel css="basis-1/4" controls={false} delay={1000} toScroll={true}>
