adfd82e2e9c1cee42b1b471b6147b3f281ddf28a
Yash Shakya <yashshakya40@gmail.com>
2025-02-04 19:32:10 +0530

    feat(Hackshetra): Added Footer

--- apps/hackshetra/src/components/Rules/Rules.jsx
+++ apps/hackshetra/src/components/Rules/Rules.jsx
@@ -3,7 +3,7 @@ import "./Rules.css"
 
 function Rules(props) {
   return (
-    <section className='flex flex-col items-center text-white w-[55%] min-h-fit my-20 shadow-[0px_0px_10px_0px_#3cb7dd] rounded-lg backdrop-blur-xl ' id='rules'>
+    <section className='flex flex-col items-center text-white w-[55%] max-md:w-[80%] min-h-fit my-14 shadow-[0px_0px_10px_0px_#3cb7dd] rounded-lg backdrop-blur-xl ' id='rules'>
       <h3 className='text-4xl max-md:text-2xl m-9 font-kode'>Rules and Guidelines</h3>
       <ul className='rules max-h-[60svh] w-[85%] relative flex flex-col items-center overflow-y-auto mb-9 '>
         {props.data.map((item) => (
