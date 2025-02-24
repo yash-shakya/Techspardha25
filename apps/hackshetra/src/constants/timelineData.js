57f25fac832fe2af8b0a3d97fb7075fa5659591f
Yash Shakya <yashshakya40@gmail.com>
2025-02-25 01:15:04 +0530

    fix(): final hackshetra commit

--- apps/hackshetra/src/constants/timelineData.js
+++ apps/hackshetra/src/constants/timelineData.js
@@ -3,56 +3,24 @@ export const TimelineData = [
         key:1,
         side:"left",
         initialX:-550,
-        date:"22th Feb 10:00 AM",
-        name:"Registration Starts",
-        detail:"Register using the register button by submitting your details."
+        date:"26 Feb 25, 05:00 PM IST",
+        name:"Idea Submission",
+        detail:"Submit the idea in the below given format: http://bit.ly/434Yvb4"
     },
     {
         key:2,
         side:"right",
         initialX:550,
-        date:"29th Feb 10:00 AM",
-        name:"Registration Ends",
-        detail:"We will be starting the ideation round right after."
+        date:"27 Feb 25, 12:00 PM IST",
+        name:"Progress Evaluation Round",
+        detail:"This will be a Offline round! You will see the “Enter” button here, once the round is live."
     },
     {
         key:3,
         side:"left",
         initialX:-550,
-        date:"29th Feb 10:00 AM",
-        name:"PPT Submission",
-        detail:"Submit your ideas in form of a ppt, including solution approach and tech stacks, at unstop."
-    },
-    {
-        key:4,
-        side:"right",
-        initialX:550,
-        date:"1st March 12:00 NOON",
-        name:"Ideation Results",
-        detail:"Selected teams can start their coding. Mentors will be assigned to selected teams."
-    },
-    {
-        key:5,
-        side:"left",
-        initialX:-550,
-        date:"07th March 10:00 AM",
-        name:"Mentor's Evaluation",
-        detail:"This will be the first progress check of the team's mentor."
-    },
-    {
-        key:6,
-        side:"right",
-        initialX:550,
-        date:"07th March 10:00 AM",
-        name:"Mentor's Evaluation Results",
-        detail:"Selected teams head for the finale. Teams need to be present on campus for the finale."
-    },
-    {
-        key:7,
-        side:"left",
-        initialX:-550,
-        date:"15th March 10:00 AM",
-        name:"Excalibur Finale",
-        detail:"Submit and present your project before our judges."
+        date:"27 Feb 25, 08:00 PM IST",
+        name:"Final Evaluation Round",
+        detail:"Presentation and pitch to the judges"
     },
 ]
